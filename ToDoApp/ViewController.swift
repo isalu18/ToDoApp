d8202d5ebb82e94592d526ae285b1279ed32ba48
Isaac Sanchez <isaacsanchez@mac-kun.local>
2022-05-04 18:13:23 -0700

    Project created plus launchscreen finished

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Isaac Sanchez on 04/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
